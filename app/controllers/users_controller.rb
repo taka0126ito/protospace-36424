741bcea0e8cd19970e497b1ade9e90b16b6fcceb
taka0126ito <taka.major@icloud.com>
2021-08-09 14:58:18 +0900

    ログイン機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :show]
 
- 
   def show
     @user = User.find(params[:id])
     @prototypes = Prototype.includes(:user)
